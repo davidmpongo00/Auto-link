f0a2a4ef937332a3960d4dc3e136decd85784f68
Ryotakashi415 <etaglocc@gmail.com>
2024-06-03 20:24:53 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,9 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
-const apiRouter = require('./routes/api');
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/api', apiRouter);
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
